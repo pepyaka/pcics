d7feea1080b3c3447449fa503849284877fe4507
Mikhail Bratchikov <m@pepyaka.org>
2022-04-15 19:01:55 +0300

    2 extended capabilites added

---
+++ src/extended_capabilities/root_complex_link_declaration.rs
@@ -0,0 +1,342 @@
+//! PCI Express Root Complex Link Declaration`
+//!
+//! The PCI Express Root Complex Link Declaration Capability is an optional Capability that is
+//! permitted to be implemented by Root Ports, RCiEPs, or RCRBs to declare a Root Complex’s
+//! internal topology.
+
+
+use core::slice;
+
+use heterob::{P4,P5,P6, endianness::{LeBytesInto, FromLeBytes}, bit_numbering::Lsb};
+
+use super::ExtendedCapabilityDataError;
+
+
+
+/// Root Complex Link Declaration
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RootComplexLinkDeclaration<'a> {
+    pub element_self_description: ElementSelfDescription,
+    pub link_entries: LinkEntries<'a>,
+}
+impl<'a> TryFrom<&'a [u8]> for RootComplexLinkDeclaration<'a> {
+    type Error = ExtendedCapabilityDataError;
+    fn try_from(slice: &'a [u8]) -> Result<Self, Self::Error> {
+        let esd_slice = &slice[..slice.len().min(ElementSelfDescription::BYTES)];
+        let element_self_description: ElementSelfDescription =
+            <[u8; ElementSelfDescription::BYTES]>::try_from(esd_slice)
+                .map_err(|_| ExtendedCapabilityDataError::DynamicSize {
+                    id: 0x0010,
+                    offset: ElementSelfDescription::BYTES,
+                    msg: "Element Self Description slice",
+                })?
+                .le_bytes_into();
+        let slice = slice.get(LinkEntries::FIRST_ENTRY_OFFSET..) 
+            .ok_or(ExtendedCapabilityDataError::DynamicSize {
+                id: 0x0010,
+                offset: LinkEntries::FIRST_ENTRY_OFFSET,
+                msg: "Link Entries slice",
+            })?;
+        
+        let end = (element_self_description.number_of_link_entries as usize) * LinkEntry::BYTES;
+        let slice = slice.get(..end)
+            .ok_or(ExtendedCapabilityDataError::DynamicSize {
+                id: 0x0010,
+                offset: end,
+                msg: "Link Entries too short",
+            })?;
+        if slice.len() < LinkEntry::BYTES {
+            Err(ExtendedCapabilityDataError::DynamicSize {
+                id: 0x0010,
+                offset: LinkEntries::FIRST_ENTRY_OFFSET,
+                msg: "should be at least one Link Entry",
+            })
+        } else {
+            Ok(Self {
+                element_self_description,
+                link_entries: LinkEntries(slice.iter()),
+            })
+        }
+    }
+}
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ElementSelfDescription {
+    /// Element Type
+    pub element_type: ElementType,
+    /// Number of Link Entries
+    pub number_of_link_entries: u8,
+    /// Component ID
+    pub component_id: u8,
+    /// Port Number
+    pub port_number: u8,
+}
+impl ElementSelfDescription {
+    pub const BYTES: usize = 4;
+}
+impl From<u32> for ElementSelfDescription {
+    fn from(dword: u32) -> Self {
+        let Lsb((
+            element_type, (), number_of_link_entries, component_id, port_number,
+        )) = P5::<_, 4, 4, 8, 8, 8>(dword).into();
+        let element_type = From::<u8>::from(element_type);
+        Self { element_type, number_of_link_entries, component_id, port_number, }
+    }
+}
+impl FromLeBytes<4> for ElementSelfDescription {
+    fn from_le_bytes(bytes: [u8; 4]) -> Self {
+        u32::from_le_bytes(bytes).into()
+    }
+}
+
+/// Indicates the type of the Root Complex Element
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ElementType {
+    /// Configuration Space Element
+    ConfigurationSpaceElement,
+    /// System Egress Port or internal sink (memory)
+    SystemEgressPortOrInternalSink,
+    /// Internal Root Complex Link
+    InternalRootComplexLink,
+    /// Reserved
+    Reserved(u8),
+}
+impl From<u8> for ElementType {
+    fn from(byte: u8) -> Self {
+        match byte {
+            0x00 => Self::ConfigurationSpaceElement,
+            0x01 => Self::SystemEgressPortOrInternalSink,
+            0x02 => Self::InternalRootComplexLink,
+            v => Self::Reserved(v),
+        }
+    }
+}
+
+
+/// Link Entries
+#[derive(Debug, Clone)]
+pub struct LinkEntries<'a>(pub slice::Iter<'a, u8>);
+impl<'a> LinkEntries<'a> {
+    pub const FIRST_ENTRY_OFFSET: usize = 0x10 - super::ECH_BYTES;
+}
+impl<'a> PartialEq for LinkEntries<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.clone().eq(other.0.clone())
+    }
+}
+impl<'a> Eq for LinkEntries<'a> {}
+impl<'a> Iterator for LinkEntries<'a> {
+    type Item = LinkEntry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut bytes = [0u8; LinkEntry::BYTES];
+        for i in 0..LinkEntry::BYTES {
+            bytes[i] = *self.0.next()?;
+        }
+        let dwords: [u32; LinkEntry::DWORDS] = bytes.le_bytes_into();
+        Some(dwords.into())
+    }
+}
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct LinkEntry {
+    /// Link Description
+    pub link_description: LinkDescription,
+    /// Link Address
+    pub link_address: LinkAddress,
+}
+impl LinkEntry {
+    pub const DWORDS: usize = 4;
+    pub const BYTES: usize = 16;
+}
+impl From<[u32; LinkEntry::DWORDS]> for LinkEntry {
+    fn from(data: [u32; LinkEntry::DWORDS]) -> Self {
+        let link_description: LinkDescription = data[0].into();
+        let link_type = link_description.link_type;
+        Self {
+            link_description,
+            link_address: LinkAddress::new(link_type, [data[2],data[3]]),
+        }
+    }
+}
+
+/// Link Description
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct LinkDescription {
+    /// Link Valid
+    pub link_valid: bool,
+    /// Link Type
+    pub link_type: u8,
+    /// Associate RCRB Header
+    pub associate_rcrb_header: bool,
+    /// Target Component ID
+    pub target_component_id: u8,
+    /// Target Port Number
+    pub target_port_number: u8,
+}
+impl From<u32> for LinkDescription {
+    fn from(dword: u32) -> Self {
+        let Lsb((
+            link_valid,
+            link_type,
+            associate_rcrb_header,
+            (),
+            target_component_id,
+            target_port_number,
+        )) = P6::<_, 1, 1, 1, 13, 8, 8>(dword).into();
+        Self {
+            link_valid,
+            link_type,
+            associate_rcrb_header,
+            target_component_id,
+            target_port_number,
+        }
+    }
+}
+impl FromLeBytes<4> for LinkDescription {
+    fn from_le_bytes(bytes: [u8;4]) -> Self { u32::from_le_bytes(bytes).into() }
+}
+
+/// Identifies the target element for the Link entry
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum LinkAddress {
+    /// Pointing to a memory-mapped RCRB
+    MemoryMappedSpace(u64),
+    /// Pointing to the Configuration Space of a Root Complex element
+    ConfigurationSpace {
+        /// Encoded number of Bus Number bits
+        n: u8,
+        /// Function Number
+        function: u8,
+        /// Device Number
+        device: u8,
+        /// Bus Number
+        bus: u8,
+        /// PCI Express Configuration Space Base Address
+        address: u64,
+    },
+}
+impl LinkAddress {
+    const DWORDS: usize = 2;
+}
+impl LinkAddress {
+    #[must_use]
+    pub fn new(link_type: u8, data: [u32; LinkAddress::DWORDS]) -> Self {
+        const BUS_AND_ADDR_OFFSET: u8 = 20;
+        let first_dword = data[0] as u64;
+        let second_dword = data[1] as u64;
+        let address = (second_dword << 32) | first_dword;
+        if link_type == 0 {
+            Self::MemoryMappedSpace(address)
+        } else {
+            let Lsb((
+                n, (), function, device
+            )) = P4::<_, 3, 9, 3, 5>(first_dword).into();
+            // with N = 000b specifying n = 8 and all other encodings specifying n = <value of N>
+            let n: u8 = if n == 0 { 8 } else { n };
+            // Bits (19 + n):20 specify the Bus Number, with 1 ≤ n ≤ 8
+            let bus = ((first_dword >> BUS_AND_ADDR_OFFSET) & !(u64::MAX << n)) as u8;
+            let laddr_mask = u64::MAX << (BUS_AND_ADDR_OFFSET + n);
+            // Bits 31:(20 + n) of the first DWORD together with the second DWORD
+            let address = address & laddr_mask;
+            Self::ConfigurationSpace { n, function, device, bus, address }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn link_description() {
+        let result: LinkDescription = 0b10101010_10101010_0000000000000_1_0_1.into();
+        let sample = LinkDescription {
+            link_valid: true,
+            link_type: 0,
+            associate_rcrb_header: true,
+            target_component_id: 0xAA,
+            target_port_number: 0xAA,
+        };
+        assert_eq!(sample, result);
+    }
+
+    #[test]
+    fn link_address_memory_mapped_space() {
+        let data = [ 0b1111_1111_1111_1111_1111_0000_0000_0000u32, 0x33221100];
+        let result = LinkAddress::new(0, data);
+        let sample = LinkAddress::MemoryMappedSpace(0x33221100FFFFF000);
+        assert_eq!(sample, result);
+    }
+
+    #[test]
+    fn link_address_configuration_space() {
+        //           address    bus  dev   fn  reserved  N
+        let data = [ 0b11111111_0101_01010_101_000000000_100u32, 0x33221100];
+        let result = LinkAddress::new(1, data);
+        let sample = LinkAddress::ConfigurationSpace {
+            n: 4,
+            function: 0b101,
+            device: 0b01010,
+            bus: 0b0101,
+            address: 0x33221100FF000000,
+        };
+        assert_eq!(sample, result);
+    }
+    
+    #[test]
+    fn link_entries() {
+        // Link0:  Desc:   TargetPort=00 TargetComponent=01 AssocRCRB- LinkType=MemMapped LinkValid+
+        //         Addr:   00000000fed19000
+        let data = [
+            0x01, 0x00, 0x01, 0x00, // Link entry 1 description
+            0x00, 0x00, 0x00, 0x00, // Link entry 1 reserved
+            0x00, 0x90, 0xd1, 0xfe, 0x00, 0x00, 0x00, 0x00, // Link entry 1 address
+        ];
+        let result = LinkEntries(data.iter()).collect::<Vec<_>>();
+
+        let sample = vec![
+            LinkEntry {
+                link_description: LinkDescription {
+                    link_valid: true,
+                    link_type: 0,
+                    associate_rcrb_header: false,
+                    target_component_id: 0x01,
+                    target_port_number: 0x00,
+                },
+                link_address: LinkAddress::MemoryMappedSpace(0x00000000fed19000),
+            }
+        ];
+        assert_eq!(sample, result);
+    }
+    
+    #[test]
+    fn parse_full_struct() {
+        // Desc:   PortNumber=02 ComponentID=01 EltType=Config
+        // Link0:  Desc:   TargetPort=00 TargetComponent=01 AssocRCRB- LinkType=MemMapped LinkValid+
+        //         Addr:   00000000fed19000
+        let data = [
+            0x05, 0x00, 0x01, 0x1c, // Root Complex Link Declaration Extended Capability Header
+            0x00, 0x01, 0x01, 0x02, // Element self description
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // reserved
+            0x01, 0x00, 0x01, 0x00, // Link entry 1 description
+            0x00, 0x00, 0x00, 0x00, // Link entry 1 reserved
+            0x00, 0x90, 0xd1, 0xfe, 0x00, 0x00, 0x00, 0x00, // Link entry 1 address
+        ];
+        let result: RootComplexLinkDeclaration = data[4..].try_into().unwrap();
+
+        let sample = RootComplexLinkDeclaration{
+            element_self_description: ElementSelfDescription {
+                element_type: ElementType::ConfigurationSpaceElement,
+                number_of_link_entries: 1,
+                component_id: 0x01,
+                port_number: 0x02,
+            },
+            link_entries: LinkEntries(data[0x10..].iter()),
+        };
+        assert_eq!(sample, result);
+    }
+}
---
+++ src/extended_capabilities/single_root_io_virtualization.rs
@@ -0,0 +1,269 @@
+//! Single Root I/O Virtualization (SR-IOV)
+//!
+//! Single Root I/O Virtualization and Sharing (SR-IOV) consists of extensions to the PCI Express
+//! (PCIe) specification suite to enable multiple System Images (SI) to share PCI hardware
+//! resources. 
+
+use core::mem::size_of;
+
+use heterob::{P16,P5,P6,P1, endianness::{FromLeBytes, LeBytesInto}, bit_numbering::Lsb};
+
+use crate::header::BaseAddressesNormal;
+use super::ExtendedCapabilityDataError;
+
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SingleRootIoVirtualization {
+    /// SR-IOV Capabilities
+    pub sriov_capabilities: SriovCapabilities,
+    /// SR-IOV Control
+    pub sriov_control: SriovControl,
+    /// SR-IOV Status
+    pub sriov_status: SriovStatus,
+    /// InitialVFs (RO)
+    pub initial_vfs: u16,
+    /// TotalVFs (RO)
+    pub total_vfs: u16,
+    /// NumVFs (RW)
+    pub num_vfs: u16,
+    /// Function Dependency Link (RO)
+    pub function_dependency_link: u8,
+    /// First VF Offset (RO)
+    pub first_vf_offset: u16,
+    /// VF Stride (RO)
+    pub vf_stride: u16,
+    /// VF Device ID (RO)
+    pub vf_device_id: u16,
+    pub page_sizes: PageSizes,
+    pub base_addresses: BaseAddressesNormal,
+    /// VF Migration State Array Offset (RO)
+    pub vf_migration_state_array_offset: u32,
+}
+impl SingleRootIoVirtualization {
+    pub const BYTES: usize = 0x40 - super::ECH_BYTES;
+}
+
+
+impl From<[u8; SingleRootIoVirtualization::BYTES]> for SingleRootIoVirtualization {
+    fn from(bytes: [u8; Self::BYTES]) -> Self {
+        let P16((
+            sriov_capabilities, sriov_control, sriov_status, initial_vfs, total_vfs, num_vfs,
+            function_dependency_link, rsvdp_0, first_vf_offset, vf_stride, rsvdp_1, vf_device_id,
+            supported_page_sizes, system_page_sizes, base_addresses, vf_migration_state_array_offset,
+        )) = bytes.le_bytes_into();
+        let _: (u8, u16, [u8; size_of::<u32>() * 6]) = (rsvdp_0, rsvdp_1, base_addresses);
+        Self {
+            sriov_capabilities,
+            sriov_control,
+            sriov_status,
+            initial_vfs,
+            total_vfs,
+            num_vfs,
+            function_dependency_link,
+            first_vf_offset,
+            vf_stride,
+            vf_device_id,
+            page_sizes: PageSizes {
+                supported: supported_page_sizes,
+                system: system_page_sizes
+            },
+            base_addresses: BaseAddressesNormal(base_addresses.le_bytes_into()),
+            vf_migration_state_array_offset,
+        }
+    }
+}
+
+impl<'a> TryFrom<&'a [u8]> for SingleRootIoVirtualization {
+    type Error = ExtendedCapabilityDataError;
+    fn try_from(slice: &'a [u8]) -> Result<Self, Self::Error> {
+        let mut array = [0; Self::BYTES];
+        let slice = slice.get(..Self::BYTES)
+            .ok_or(ExtendedCapabilityDataError::FixedSize {
+                id: 0x0010,
+                expected: Self::BYTES,
+                found: slice.len(),
+            })?;
+        array.clone_from_slice(slice);
+        Ok(array.into())
+    }
+}
+
+/// SR-IOV Capabilities
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SriovCapabilities {
+    /// VF Migration Capable
+    pub vf_migration_capable: bool,
+    /// ARI Capable Hierarchy Preserved
+    pub ari_capable_hierarchy_preserved: bool,
+    /// VF 10-Bit Tag Requester Supported
+    pub vf_10bit_tag_requester_supported: bool,
+    /// VF Migration Interrupt Message Number
+    pub vf_migration_interrupt_message_number: u16,
+}
+impl From<u32> for SriovCapabilities {
+    fn from(dword: u32) -> Self {
+        let Lsb((
+            vf_migration_capable,
+            ari_capable_hierarchy_preserved,
+            vf_10bit_tag_requester_supported,
+            (),
+            vf_migration_interrupt_message_number,
+        )) = P5::<_, 1, 1, 1, 18, 11>(dword).into();
+        Self {
+            vf_migration_capable,
+            ari_capable_hierarchy_preserved,
+            vf_10bit_tag_requester_supported,
+            vf_migration_interrupt_message_number,
+        }
+    }
+}
+impl FromLeBytes<4> for SriovCapabilities {
+    fn from_le_bytes(bytes: [u8;4]) -> Self { u32::from_le_bytes(bytes).into() }
+}
+
+/// SR-IOV Control
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SriovControl {
+    /// VF Enable
+    pub vf_enable: bool,
+    /// VF Migration Enable
+    pub vf_migration_enable: bool,
+    /// VF Migration Interrupt Enable
+    pub vf_migration_interrupt_enable: bool,
+    /// VF MSE
+    pub vf_mse: bool,
+    /// ARI Capable Hierarchy – PCI Express Endpoint
+    pub ari_capable_hierarchy: bool,
+    /// VF 10-Bit Tag Requester Enable
+    pub vf_10bit_tag_requester_enable: bool,
+}
+impl From<u16> for SriovControl {
+    fn from(word: u16) -> Self {
+        let Lsb((
+            vf_enable,
+            vf_migration_enable,
+            vf_migration_interrupt_enable,
+            vf_mse,
+            ari_capable_hierarchy,
+            vf_10bit_tag_requester_enable,
+        )) = P6::<_, 1, 1, 1, 1, 1, 1>(word).into();
+        Self {
+            vf_enable,
+            vf_migration_enable,
+            vf_migration_interrupt_enable,
+            vf_mse,
+            ari_capable_hierarchy,
+            vf_10bit_tag_requester_enable,
+        }
+    }
+}
+impl FromLeBytes<2> for SriovControl {
+    fn from_le_bytes(bytes: [u8;2]) -> Self { u16::from_le_bytes(bytes).into() }
+}
+
+/// SR-IOV Status
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SriovStatus {
+    /// VF Migration Status
+    pub vf_migration_status: bool,
+}
+impl From<u16> for SriovStatus {
+    fn from(word: u16) -> Self {
+        let Lsb((
+            vf_migration_status,
+        )) = P1::<_, 1>(word).into();
+        Self {
+            vf_migration_status,
+        }
+    }
+}
+impl FromLeBytes<2> for SriovStatus {
+    fn from_le_bytes(bytes: [u8;2]) -> Self { u16::from_le_bytes(bytes).into() }
+}
+
+
+/// Defines the page size the system will use to map the VFs’ memory addresses
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct PageSizes {
+    /// Supported Page Sizes (RO)
+    pub supported: u32,
+    /// System Page Size (RW)
+    pub system: u32,
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::header::{BaseAddress, BaseAddressType};
+    use pretty_assertions::assert_eq;
+
+    // IOVCap: Migration-, Interrupt Message Number: 000
+    // IOVCtl: Enable- Migration- Interrupt- MSE- ARIHierarchy-
+    // IOVSta: Migration-
+    // Initial VFs: 8, Total VFs: 8, Number of VFs: 0, Function Dependency Link: 00
+    // VF offset: 384, stride: 4, Device ID: 1520
+    // Supported Page Size: 00000553, System Page Size: 00000001
+    // Region 0: Memory at 00000000a0180000 (64-bit, prefetchable)
+    // Region 3: Memory at 00000000a01a0000 (64-bit, prefetchable)
+    const DATA: [u8; 60] = [
+                            0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x08,0x00,
+        0x00,0x00,0x00,0x00,0x80,0x01,0x04,0x00,0x00,0x00,0x20,0x15,0x53,0x05,0x00,0x00,
+        0x01,0x00,0x00,0x00,0x0c,0x00,0x18,0xa0,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+        0x0c,0x00,0x1a,0xa0,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+    ];
+
+    #[test]
+    fn parse_full_struct() {
+        let result: SingleRootIoVirtualization = DATA.into();
+    
+        let sample = SingleRootIoVirtualization {
+            sriov_capabilities: SriovCapabilities {
+                vf_migration_capable: false,
+                ari_capable_hierarchy_preserved: true,
+                vf_10bit_tag_requester_supported: false,
+                vf_migration_interrupt_message_number: 0,
+            },
+            sriov_control: SriovControl {
+                vf_enable: false,
+                vf_migration_enable: false,
+                vf_migration_interrupt_enable: false,
+                vf_mse: false,
+                ari_capable_hierarchy: false,
+                vf_10bit_tag_requester_enable: false,
+            },
+            sriov_status: SriovStatus {
+                vf_migration_status: false
+            },
+            initial_vfs: 8,
+            total_vfs: 8,
+            num_vfs: 0,
+            function_dependency_link: 0,
+            first_vf_offset: 384,
+            vf_stride: 4,
+            vf_device_id: 0x1520,
+            page_sizes: PageSizes {
+                supported: 0x00000553,
+                system: 0x00000001,
+            },
+            base_addresses: [
+                BaseAddress {
+                    region: 0,
+                    base_address_type: BaseAddressType::MemorySpace64 {
+                        prefetchable: true, base_address: 0xa0180000
+                    }, 
+                },
+                BaseAddress {
+                    region: 3,
+                    base_address_type: BaseAddressType::MemorySpace64 {
+                        prefetchable: true, base_address: 0xa01a0000
+                    }, 
+                },
+            ].iter().collect::<BaseAddressesNormal>().into(),
+            vf_migration_state_array_offset: 0,
+        };
+        
+        assert_eq!(sample, result);
+    }
+}
