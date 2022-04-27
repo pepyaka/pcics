913ca97b369f8afe3a63b1edfa1c63fe67152933
Mikhail Bratchikov <m@pepyaka.org>
2022-04-27 15:39:15 +0300

    Migrate VendorSpecific from byte to heterob

--- src/capabilities/vendor_specific.rs
+++ src/capabilities/vendor_specific.rs
@@ -7,14 +7,18 @@
 //! manufacturing steps as either a 32-bit or 64-bit PCI agent and the Vendor Specific capability
 //! structure tells the device driver which features the device supports. 
 
-use byte::{
-    ctx::*,
-    self,
-    TryRead,
-    // TryWrite,
-    BytesExt,
-};
+use snafu::prelude::*;
+use heterob::{endianness::Le, P5};
 
+#[derive(Snafu, Debug, Clone, PartialEq, Eq)]
+pub enum VendorSpecificError {
+    #[snafu(display("length should be > 0"))]
+    Length,
+    #[snafu(display("unable to get {size} bytes data"))]
+    Data { size: usize },
+    #[snafu(display("Virtio size shold be > 12"))]
+    Virtio,
+}
 
 /// Only vendor-specific data length. Without Cap ID, Next Ptr and length itself
 #[derive(Debug, PartialEq, Eq)]
@@ -25,25 +29,21 @@ impl<'a> VendorSpecific<'a> {
     }
     pub fn vendor_capability(&self, vendor_id: u16, device_id: u16) -> VendorCapabilty<'a> {
         let data = self.0;
-        let offset = &mut 0;
-        match (vendor_id, device_id) {
-            (0x1af4, 0x1000..=0x107f) => {
-                if let Ok(virtio) = data.read_with::<Virtio>(offset, LE) {
-                    VendorCapabilty::Virtio(virtio)
-                } else {
-                    VendorCapabilty::Unspecified(data)
-                }
-            },
-            _ => VendorCapabilty::Unspecified(data),
+        if let (0x1af4, 0x1000..=0x107f, Ok(v)) = (vendor_id, device_id, data.try_into()) {
+            VendorCapabilty::Virtio(v)
+        } else {
+            VendorCapabilty::Unspecified(data)
         }
     }
 }
-impl<'a> TryRead<'a, Endian> for VendorSpecific<'a> {
-    fn try_read(bytes: &'a [u8], endian: Endian) -> byte::Result<(Self, usize)> {
-        let offset = &mut 0;
-        let length = bytes.read_with::<u8>(offset, endian)? as usize - 1;
-        let vs = VendorSpecific(bytes.read_with(offset, Bytes::Len(length))?);
-        Ok((vs, *offset))
+impl<'a> TryFrom<&'a [u8]> for VendorSpecific<'a> {
+    type Error = VendorSpecificError;
+    fn try_from(slice: &'a [u8]) -> Result<Self, Self::Error> {
+        let size: usize = slice.get(0).filter(|&&l| l > 0).copied()
+            .ok_or(VendorSpecificError::Length)?.into();
+        let data = slice.get(1..(size + 1))
+            .ok_or(VendorSpecificError::Data { size })?;
+        Ok(VendorSpecific(data))
     }
 }
 
@@ -52,22 +52,6 @@ impl<'a> TryRead<'a, Endian> for VendorSpecific<'a> {
 pub enum VendorCapabilty<'a> {
     Unspecified(&'a [u8]),
     Virtio(Virtio),
-}
-impl<'a> VendorCapabilty<'a> {
-    /// Vendor Specific capability depends on Vendor ID and Device ID
-    pub fn new(data: &'a [u8], vendor_id: u16, device_id: u16) -> Self {
-        let offset = &mut 0;
-        match (vendor_id, device_id) {
-            (0x1af4, 0x1000..=0x107f) => {
-                if let Ok(virtio) = data.read_with::<Virtio>(offset, LE) {
-                    Self::Virtio(virtio)
-                } else {
-                    Self::Unspecified(data)
-                }
-            },
-            _ => Self::Unspecified(data),
-        }
-    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -83,25 +67,35 @@ pub enum Virtio {
     DeviceCfg { bar: u8, offset: u32, size: u32 },
     Unknown { bar: u8, offset: u32, size: u32 },
 }
-impl<'a> TryRead<'a, Endian> for Virtio {
-    fn try_read(bytes: &'a [u8], endian: Endian) -> byte::Result<(Self, usize)> {
-        let offset_ = &mut 0;
-        let type_ = bytes.read_with::<u8>(offset_, endian)?;
-        let bar = bytes.read_with::<u8>(offset_, endian)?;
-        *offset_ += 3; // aligned by u32 ?
-        let offset = bytes.read_with::<u32>(offset_, endian)?;
-        let size = bytes.read_with::<u32>(offset_, endian)?;
-        let virtio = match type_ {
-            1 => Self::CommonCfg { bar, offset, size },
+impl Virtio {
+    /// Identifies the structure: [u8]
+    /// [u8]: Where to find it: [u8]
+    /// [[u8;3]]: Pad to full dword: [[u8;3]]
+    /// Offset within bar: [u32]
+    /// Length of the structure, in bytes: [u32]
+    pub const SIZE: usize = 1 + 1 + 3 + 4 + 4; // 13 bytes
+}
+impl<'a> TryFrom<&'a [u8]> for Virtio {
+    type Error = VendorSpecificError;
+    fn try_from(slice: &'a [u8]) -> Result<Virtio, Self::Error> {
+        let bytes = slice.get(..Virtio::SIZE)
+            .and_then(|slice| <[u8;Virtio::SIZE]>::try_from(slice).ok())
+            .ok_or(VendorSpecificError::Virtio)?;
+        let Le((cfg_type, bar, padding, offset, size)) = P5(bytes).into();
+        let _: [u8;3] = padding;
+        let result = match cfg_type {
+            1u8 => Self::CommonCfg { bar, offset, size },
             2 => {
-                let multiplier = bytes.read_with::<u32>(offset_, endian).ok();
+                let multiplier = slice.get(Virtio::SIZE .. Virtio::SIZE + 4)
+                    .and_then(|slice| <[u8;4]>::try_from(slice).ok())
+                    .map(u32::from_le_bytes);
                 Self::Notify { bar, offset, size, multiplier }
             },
             3 => Self::Isr { bar, offset, size },
             4 => Self::DeviceCfg { bar, offset, size },
             _ => Self::Unknown { bar, offset, size },
         };
-        Ok((virtio, *offset_))
+        Ok(result)
     }
 }
 
@@ -111,7 +105,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn vendor_capability() {
+    fn virtio() {
         // Capabilities: [b4] Vendor Specific Information: VirtIO: Notify
         //         BAR=4 offset=00003000 size=00001000 multiplier=00000004
         let data = [
@@ -125,10 +119,11 @@ mod tests {
             0x00, 0x10, 0x00, 0x00, // size
             0x04, 0x00, 0x00, 0x00  // multiplier
         ];
-        let vc = VendorCapabilty::new(&data[3..], 0x1af4, 0x1045);
-        let sample = VendorCapabilty::Virtio(
-            Virtio::Notify { bar: 4, offset: 0x00003000, size: 0x00001000, multiplier: Some(0x00000004) }
-        );
+        let vc: Virtio = data[3..].try_into().unwrap();
+        let sample = Virtio::Notify {
+            bar: 4, offset: 0x00003000, size: 0x00001000,
+            multiplier: Some(0x00000004)
+        };
         assert_eq!(sample, vc);
     }
 }
